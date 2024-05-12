8a22590db15a55ff23b92fce0ebe50f1077e9fe6
harojortiz <144560786+harojortiz@users.noreply.github.com>
2024-05-11 20:44:04 -0500

    go_router_configurado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app_new/config/menu/menu_items.dart';
-import 'package:widgets_app_new/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -54,7 +54,8 @@ class _CustomListTile extends StatelessWidget {
         //    builder: (context) => const ButtonsScreen(),
         //  ),
         //);
-        Navigator.pushNamed(context, menuItem.link);
+        //Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,7 @@
+
+
+export 'package:widgets_app_new/presentation/screens/buttons/buttons_screen.dart';
+export 'package:widgets_app_new/presentation/screens/cards/cards_screen.dart';
+export 'package:widgets_app_new/presentation/screens/home/home_screen.dart';
+
+
